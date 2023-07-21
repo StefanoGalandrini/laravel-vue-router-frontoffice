57df3267d1e5aae69c76d8b26c136c6525a7a1b1
StefanoGalandrini <stefe68@gmail.com>
2023-07-22 01:00:47 +0200

    Add filter and fix style

---
+++ src/pages/AppFilter.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+	data() {
+		return {
+			drinkTypes: [
+				{
+					name: "All",
+					value: 1,
+				},
+				{
+					name: "Alcoholic",
+					value: 2,
+				},
+				{
+					name: "Non alcoholic",
+					value: 3,
+				},
+			],
+		};
+	},
+
+	// methods: {
+	// 	updateFilter(event) {
+	// 		this.$emit("changeType", event.target.value);
+	// 	},
+	// },
+};
+</script>
+
+<template>
+	<select
+		class="form-select w-25"
+		@change="$emit('changeType', parseInt($event.target.value))">
+		<!-- <option selected disabled>Choose</option> -->
+		<option
+			class="option"
+			v-for="dtype in drinkTypes"
+			:key="dtype.value"
+			:value="dtype.value">
+			{{ dtype.name }}
+		</option>
+	</select>
+</template>
+
+<style lang="scss" scoped>
+select {
+	margin: 1.75rem 0;
+	display: flex;
+	align-items: center;
+	font-size: 1.25rem;
+
+	.option {
+		font-size: 1rem;
+	}
+}
+</style>
